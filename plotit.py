6ddfc138f6245f395efb308d518974b58947f397
Peter Reintjes <peter.reintjes@ncmls.org>
2017-03-07 14:49:46 -0500

    new plot/gather

--- plotit.py
+++ plotit.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python -u
+# Length of emlst represents 360-700nM
+# Pad with 350nM ?
 from __future__ import print_function
 import os,sys
 import matplotlib.pyplot as plt
 import time
 f = open(sys.argv[1],'r')
-inlst = []
+emlst = []
+exlst = []
+mslst = []
 for l in f.readlines() :
-    inlst.append(l.split('\t')[1])
-llen = len(inlst)
-
-lst = []
+    (tm, ex, em) = l.rstrip().split('\t')
+    mslst.append(int(tm))
+    exlst.append(int(ex))
+    emlst.append(int(em))
+llen = len(emlst)
+outlst = []
 for i in range(llen):
-    lst.append(0)
-lst.extend(inlst)
-llen = len(lst)
-
-lams = []
-for i in range(llen) :
-    lams.append(i*720/llen)
-
-plt.plot(lams, lst)
-plt.axis([0,720,0,100])
+    outlst.append(0)
+outlst.extend(emlst)
+olen = len(outlst)
+step = 700.0/olen
+print("step is "+str(step))
+tmlst = []
+indx = 0.0
+for i in range(olen) :
+    tmlst.append(indx)
+    indx = indx + step
+plt.plot(tmlst, outlst)
+plt.axis([0,700,0, 6*max(emlst)/5])
 plt.xlabel('Wavelength')
 plt.ylabel('Raman Spectrum of De-Ionized Water')
 plt.show()
--- waters.ino
+++ waters.ino
@@ -4,14 +4,27 @@ void setup()
 {
 	Serial.begin(9600);
 	delay(1000);
-	Serial.println("EXCITE\tFlUOR");
 	analogReference(INTERNAL);
+	delay(100);
+}
+
+void show(int ex, int em)
+{
+	Serial.print(millis());
+	Serial.print("\t");
+	Serial.print(ex);
+	Serial.print("\t");
+	Serial.println(em);
+	delay(10);
 }
 
 void loop()
 {
-	Serial.print(analogRead(EXCITE));
-	Serial.print("\t");
-	Serial.println(analogRead(FLUOR));
-	delay(100);
+static boolean once = false;
+
+    if (once) {
+        Serial.println("TIME\tEXCITE\tFlUOR");
+	once = false;
+    }
+    show(analogRead(EXCITE), analogRead(FLUOR));
 }
