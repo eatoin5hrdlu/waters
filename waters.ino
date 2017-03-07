e17ff2962d37cba8698686ccae71861a954496a2
Peter Reintjes <peter.reintjes@ncmls.org>
2017-03-07 18:14:56 -0500

    new waters.ino

--- waters.ino
+++ waters.ino
@@ -1,8 +1,19 @@
 #include "params.h"
+extern volatile unsigned long timer0_millis;
+boolean streaming = false;
+
+void setMillis(unsigned long new_millis)
+{
+  uint8_t oldSREG = SREG;
+  cli();
+  timer0_millis = new_millis;
+  SREG = oldSREG;
+}
 
 void setup()
 {
 	Serial.begin(9600);
+	pinMode(START,OUTPUT);
 	delay(1000);
 	analogReference(INTERNAL);
 	delay(100);
@@ -15,16 +26,30 @@ void show(int ex, int em)
 	Serial.print(ex);
 	Serial.print("\t");
 	Serial.println(em);
-	delay(10);
+	delay(30);
+}
+/*
+ * 1) Print HEADING
+ * 2) Send Program Start
+ * 3) Stream data for programmed interval
+ */
+
+void startStream(void)
+{
+	Serial.println("\n\nTIME\tEXCITE\tEMISSION\n");
+	digitalWrite(START,1);
+	delay(300);
+	digitalWrite(START,0);
+	setMillis(0L);
+	streaming = true;
 }
 
 void loop()
 {
-static boolean once = false;
+    if (streaming)
+       show(analogRead(EXCITE), analogRead(FLUOR));
+    else
+	startStream();
 
-    if (once) {
-        Serial.println("TIME\tEXCITE\tFlUOR");
-	once = false;
-    }
-    show(analogRead(EXCITE), analogRead(FLUOR));
+    if (millis() > 180000L) streaming = false;
 }
